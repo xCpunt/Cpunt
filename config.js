4e58f0a1d37efb72408691cec33a2ff542cb74d3
xCpunt <xavi@contrapunt.coop>
2026-04-13 13:05:17 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbypQo-Ko1jWNok8AiG5AspN6MHi-G5Df13Tp0tihHWbmdgaF5da8e-84-5OXoZMrGrw/exec",
+  APP_NAME: "Projectes",
+  SESSION_HOURS: 8
+};
